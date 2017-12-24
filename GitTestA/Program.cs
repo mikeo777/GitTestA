b06df55d9b3e0c1e9b361627104e878fda107ff0
Michael Offner <michaeloffner@hotmail.com>
2017-12-23 20:49:49 -0500

    Another change.

--- GitTestA/Program.cs
+++ GitTestA/Program.cs
@@ -8,7 +8,7 @@ namespace GitTestA
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();  
+            Console.ReadKey();   
         }
     }
 }
